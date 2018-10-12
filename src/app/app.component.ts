79f388289b5327e6469f6610dc0b94b3ecf3073f
sebastian tabera <ltabera@sumset.com>
2018-10-12 17:39:26 -0500

    tuberias iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Fernando';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a = 0.236;
 }
